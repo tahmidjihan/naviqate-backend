62c3ba200905f1d4da762d0d0778e0bf37c7fbc6
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-07-17 21:49:31 +0600

    factoring the shapes

--- Functions/getUsers.js
+++ Functions/getUsers.js
@@ -1,4 +1,4 @@
-import supabase from './supabaseClient.js';
+import supabase from '../supabaseClient.js';
 
 const getUsers = async () => {
   const { data, error } = await supabase.from('users').select('*');
