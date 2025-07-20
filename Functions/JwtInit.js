b1c8ec5a9e4d4661d18d2a79fed5e2f5ca1b6553
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-07-20 07:03:03 +0600

    made token verify more accurate

--- Functions/JwtInit.js
+++ Functions/JwtInit.js
@@ -1,7 +1,22 @@
 import jwt from 'jsonwebtoken';
-
-export const createToken = (data) => {
-  return jwt.sign(data, process.env.JWT_SECRET);
+import admin from '../admin.js';
+export const createToken = async (
+  idTokenRaw,
+  data = { role: null, company: null }
+) => {
+  const idToken = idTokenRaw.split('Bearer ')[1];
+  try {
+    const { role, company } = data;
+    const decoded = await admin.auth().verifyIdToken(idToken);
+    const { email, name, uid } = decoded;
+    const token = jwt.sign(
+      { email, name, uid, role, company },
+      process.env.JWT_SECRET
+    );
+    return token;
+  } catch (error) {
+    return 'no or invalid token';
+  }
 };
 
 export const decodeToken = (token) => {
@@ -9,6 +24,9 @@ export const decodeToken = (token) => {
 };
 
 export const isTokenValid = (token) => {
+  if (!token) {
+    return false;
+  }
   try {
     jwt.verify(token, process.env.JWT_SECRET);
     return true;
