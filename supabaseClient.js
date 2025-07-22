689eaf6ab65a0d2cc05042ebeec120b6275bd11f
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-07-22 21:36:39 +0600

    removed a buggy thing

--- supabaseClient.js
+++ supabaseClient.js
@@ -3,8 +3,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabase = createClient(
-  process.env.SUPABASE_URL, // e.g. https://urmtsdminiolmxajhbrt.supabase.co
-  process.env.SUPABASE_SERVICE_KEY // service_role secret (from Supabase settings)
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_KEY
 );
 
 export default supabase;
