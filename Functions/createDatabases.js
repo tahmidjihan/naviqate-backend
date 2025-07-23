d15851b3c709652809c35b299a6a2efe4c2f0772
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-07-23 19:46:04 +0600

    remade createDatabases

--- Functions/createDatabases.js
+++ Functions/createDatabases.js
@@ -1,7 +1,20 @@
 import supabase from '../supabaseClient.js';
 
-export const createDatabases = async (name, created_by, company) => {
-  const { data, error } = await supabase.from('databases').insert([]);
+export const createDatabases = async (
+  name,
+  created_by,
+  company,
+  company_id
+) => {
+  const { data, error } = await supabase.from('databases').insert([
+    {
+      name: name,
+      created_by: created_by,
+      company: company,
+      company_id: company_id,
+      updated_at: new Date().toISOString(),
+    },
+  ]);
   if (error) {
     return 'Supabase error:', error;
   } else {
