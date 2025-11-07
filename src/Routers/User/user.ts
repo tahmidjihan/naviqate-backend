ba73bb0901ad37903216bf641f16a9097f8e9ade
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-11-07 19:11:08 +0600

    create User

--- src/Routers/User/user.ts
+++ src/Routers/User/user.ts
@@ -14,13 +14,28 @@ router.get('/', async (req, res) => {
   console.log('Hello user!');
   res.send('Hello user!');
 });
-router.get('/loginTahmidJihan', async (req, res) => {
-  let { data, error } = await supabase.auth.signInWithPassword({
-    email: process.env.DEMO_USER,
-    password: process.env.DEMO_USER_PASSWORD,
+// router.get('/loginTahmidJihan', async (req, res) => {
+//   let { data, error } = await supabase.auth.signInWithPassword({
+//     email: process.env.DEMO_USER,
+//     password: process.env.DEMO_USER_PASSWORD,
+//   });
+//   console.log(data, error);
+//   res.send(data);
+// });
+router.post('/create', async (req, res) => {
+  // console.log(await req.body);
+  // res.send(await req.body);
+  const { data, error } = await supabase.auth.signUp({
+    email: req.body.email,
+    password: req.body.password,
   });
-  console.log(data, error);
-  res.send(data);
+  // console.log(data, error);
+  // res.send(data);
+  if (data) {
+    res.send(data);
+  } else {
+    res.send(error);
+  }
 });
 const userRouter = router;
 export default userRouter;
