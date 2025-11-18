6674955df6a7709b2b9b2522568be574308716be
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-11-19 03:21:17 +0600

    .

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+// import { router } from './routes';
+import userRouter from './Routers/User/user.ts';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+app.use('/user', userRouter);
+// app.get('/user', (req, res) => {
+//   console.log('Hello user!');
+//   res.send('Hello user!');
+// });
+
+app.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
+});
