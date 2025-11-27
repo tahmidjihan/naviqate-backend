b48a8d96e11b6c9e573d711c1b24083f5132b77b
Tahmid Jihan <tahmidjihan321@gmail.com>
2025-11-28 01:13:32 +0600

    .

--- src/Routes/Dashboard/index.ts
+++ src/Routes/Dashboard/index.ts
@@ -7,5 +7,7 @@ router.get('/', (req, res) => {
   res.send('Welcome to the Dashboard!');
 });
 router.use('/blogs', await import('./blogs.ts').then((m) => m.default));
+router.use('/company', await import('./company.ts').then((m) => m.default));
+router.use('/inbox', await import('./inbox.ts').then((m) => m.default));
 
 export default router;
